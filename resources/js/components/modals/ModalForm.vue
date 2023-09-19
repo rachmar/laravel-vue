6319d62db003f396db3961dd6bca8593f5167ab8
Rachmar Mohammad <rachmar.dev@gmail.com>
2023-09-19 13:35:17 +0800

    add modals

--- resources/js/components/modals/ModalForm.vue
+++ resources/js/components/modals/ModalForm.vue
@@ -7,30 +7,31 @@ const { modalTitle, modalModule } = defineProps(['modalTitle', 'modalModule']);
 </script>
 
 <template>
-    <div class="modal-overlay" v-if="modalModule.currState()">
-        <div class="modal fade show" id="modal-form" aria-modal="true" role="dialog">
-            <div class="modal-dialog modal-lg">
-                <div class="modal-content">
-                    <div class="modal-header">
-                        <h5 class="modal-title">{{ modalModule.currTitle() }}</h5>
-                        <button type="button" class="close" @click="modalModule.closeModal()" aria-label="Close">
-                            <span aria-hidden="true">×</span>
-                        </button>
-                    </div>
-                    <div class="modal-body">
-                        <slot></slot>
-                        <button @click="modalModule.closeModal()">Close Modal</button>
+    <Transition>
+        <div class="modal-overlay" v-if="modalModule.currState()">
+            <div class="modal fade show" id="modal-form" aria-modal="true" role="dialog">
+                <div class="modal-dialog modal-lg">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <h5 class="modal-title">{{ modalModule.currTitle() }}</h5>
+                            <button type="button" class="btn btn-danger btn-sm" @click="modalModule.closeModal()">
+                                Close
+                            </button>
+                        </div>
+                        <div class="modal-body">
+                            <slot></slot>
+                            <button @click="modalModule.closeModal()">Close Modal</button>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
-    </div>
+    </Transition>
 </template>
 
 <style scoped>
 .modal {
     display: block;
-    overflow-y: auto;
 }
 
 .modal-overlay {
@@ -43,6 +44,16 @@ const { modalTitle, modalModule } = defineProps(['modalTitle', 'modalModule']);
     z-index: 9999;
     align-items: center;
     justify-content: center;
+}
+
+.v-enter-active,
+.v-leave-active {
+    transition: opacity 0.4s ease;
+}
+
+.v-enter-from,
+.v-leave-to {
+    opacity: 0;
 }
 </style>
 
--- resources/js/pages/Main.vue
+++ resources/js/pages/Main.vue
@@ -48,14 +48,13 @@ onMounted(async () => {
       <ModalForm :modalModule="modalModule">
         <form>
           <div class="row">
-            <div class="col-sm-6">
-
+            <div class="col-md-12">
               <div class="form-group">
                 <label>Text</label>
                 <input type="text" class="form-control" placeholder="Enter ...">
               </div>
             </div>
-            <div class="col-sm-6">
+            <div class="col-md-12">
               <div class="form-group">
                 <label>Text Disabled</label>
                 <input type="text" class="form-control" placeholder="Enter ..." disabled="">
@@ -63,123 +62,198 @@ onMounted(async () => {
             </div>
           </div>
           <div class="row">
-            <div class="col-sm-6">
-
+            <div class="col-md-12">
               <div class="form-group">
-                <label>Textarea</label>
-                <textarea class="form-control" rows="3" placeholder="Enter ..."></textarea>
+                <label>Text</label>
+                <input type="text" class="form-control" placeholder="Enter ...">
               </div>
             </div>
-            <div class="col-sm-6">
+            <div class="col-md-12">
               <div class="form-group">
-                <label>Textarea Disabled</label>
-                <textarea class="form-control" rows="3" placeholder="Enter ..." disabled=""></textarea>
+                <label>Text Disabled</label>
+                <input type="text" class="form-control" placeholder="Enter ..." disabled="">
               </div>
             </div>
-          </div>
-
-          <div class="form-group">
-            <label class="col-form-label" for="inputSuccess"><i class="fas fa-check"></i> Input with
-              success</label>
-            <input type="text" class="form-control is-valid" id="inputSuccess" placeholder="Enter ...">
-          </div>
-          <div class="form-group">
-            <label class="col-form-label" for="inputWarning"><i class="far fa-bell"></i> Input with
-              warning</label>
-            <input type="text" class="form-control is-warning" id="inputWarning" placeholder="Enter ...">
-          </div>
-          <div class="form-group">
-            <label class="col-form-label" for="inputError"><i class="far fa-times-circle"></i> Input with
-              error</label>
-            <input type="text" class="form-control is-invalid" id="inputError" placeholder="Enter ...">
-          </div>
-          <div class="row">
-            <div class="col-sm-6">
-
+          </div><div class="row">
+            <div class="col-md-12">
               <div class="form-group">
-                <div class="form-check">
-                  <input class="form-check-input" type="checkbox">
-                  <label class="form-check-label">Checkbox</label>
-                </div>
-                <div class="form-check">
-                  <input class="form-check-input" type="checkbox" checked="">
-                  <label class="form-check-label">Checkbox checked</label>
-                </div>
-                <div class="form-check">
-                  <input class="form-check-input" type="checkbox" disabled="">
-                  <label class="form-check-label">Checkbox disabled</label>
-                </div>
+                <label>Text</label>
+                <input type="text" class="form-control" placeholder="Enter ...">
               </div>
             </div>
-            <div class="col-sm-6">
-
+            <div class="col-md-12">
               <div class="form-group">
-                <div class="form-check">
-                  <input class="form-check-input" type="radio" name="radio1">
-                  <label class="form-check-label">Radio</label>
-                </div>
-                <div class="form-check">
-                  <input class="form-check-input" type="radio" name="radio1" checked="">
-                  <label class="form-check-label">Radio checked</label>
-                </div>
-                <div class="form-check">
-                  <input class="form-check-input" type="radio" disabled="">
-                  <label class="form-check-label">Radio disabled</label>
-                </div>
+                <label>Text Disabled</label>
+                <input type="text" class="form-control" placeholder="Enter ..." disabled="">
               </div>
             </div>
-          </div>
-          <div class="row">
-            <div class="col-sm-6">
-
+          </div><div class="row">
+            <div class="col-md-12">
               <div class="form-group">
-                <label>Select</label>
-                <select class="form-control">
-                  <option>option 1</option>
-                  <option>option 2</option>
-                  <option>option 3</option>
-                  <option>option 4</option>
-                  <option>option 5</option>
-                </select>
+                <label>Text</label>
+                <input type="text" class="form-control" placeholder="Enter ...">
               </div>
             </div>
-            <div class="col-sm-6">
+            <div class="col-md-12">
               <div class="form-group">
-                <label>Select Disabled</label>
-                <select class="form-control" disabled="">
-                  <option>option 1</option>
-                  <option>option 2</option>
-                  <option>option 3</option>
-                  <option>option 4</option>
-                  <option>option 5</option>
-                </select>
+                <label>Text Disabled</label>
+                <input type="text" class="form-control" placeholder="Enter ..." disabled="">
               </div>
             </div>
-          </div>
-          <div class="row">
-            <div class="col-sm-6">
-
+          </div><div class="row">
+            <div class="col-md-12">
               <div class="form-group">
-                <label>Select Multiple</label>
-                <select multiple="" class="form-control">
-                  <option>option 1</option>
-                  <option>option 2</option>
-                  <option>option 3</option>
-                  <option>option 4</option>
-                  <option>option 5</option>
-                </select>
+                <label>Text</label>
+                <input type="text" class="form-control" placeholder="Enter ...">
               </div>
             </div>
-            <div class="col-sm-6">
+            <div class="col-md-12">
               <div class="form-group">
-                <label>Select Multiple Disabled</label>
-                <select multiple="" class="form-control" disabled="">
-                  <option>option 1</option>
-                  <option>option 2</option>
-                  <option>option 3</option>
-                  <option>option 4</option>
-                  <option>option 5</option>
-                </select>
+                <label>Text Disabled</label>
+                <input type="text" class="form-control" placeholder="Enter ..." disabled="">
+              </div>
+            </div>
+          </div><div class="row">
+            <div class="col-md-12">
+              <div class="form-group">
+                <label>Text</label>
+                <input type="text" class="form-control" placeholder="Enter ...">
+              </div>
+            </div>
+            <div class="col-md-12">
+              <div class="form-group">
+                <label>Text Disabled</label>
+                <input type="text" class="form-control" placeholder="Enter ..." disabled="">
+              </div>
+            </div>
+          </div><div class="row">
+            <div class="col-md-12">
+              <div class="form-group">
+                <label>Text</label>
+                <input type="text" class="form-control" placeholder="Enter ...">
+              </div>
+            </div>
+            <div class="col-md-12">
+              <div class="form-group">
+                <label>Text Disabled</label>
+                <input type="text" class="form-control" placeholder="Enter ..." disabled="">
+              </div>
+            </div>
+          </div><div class="row">
+            <div class="col-md-12">
+              <div class="form-group">
+                <label>Text</label>
+                <input type="text" class="form-control" placeholder="Enter ...">
+              </div>
+            </div>
+            <div class="col-md-12">
+              <div class="form-group">
+                <label>Text Disabled</label>
+                <input type="text" class="form-control" placeholder="Enter ..." disabled="">
+              </div>
+            </div>
+          </div><div class="row">
+            <div class="col-md-12">
+              <div class="form-group">
+                <label>Text</label>
+                <input type="text" class="form-control" placeholder="Enter ...">
+              </div>
+            </div>
+            <div class="col-md-12">
+              <div class="form-group">
+                <label>Text Disabled</label>
+                <input type="text" class="form-control" placeholder="Enter ..." disabled="">
+              </div>
+            </div>
+          </div><div class="row">
+            <div class="col-md-12">
+              <div class="form-group">
+                <label>Text</label>
+                <input type="text" class="form-control" placeholder="Enter ...">
+              </div>
+            </div>
+            <div class="col-md-12">
+              <div class="form-group">
+                <label>Text Disabled</label>
+                <input type="text" class="form-control" placeholder="Enter ..." disabled="">
+              </div>
+            </div>
+          </div><div class="row">
+            <div class="col-md-12">
+              <div class="form-group">
+                <label>Text</label>
+                <input type="text" class="form-control" placeholder="Enter ...">
+              </div>
+            </div>
+            <div class="col-md-12">
+              <div class="form-group">
+                <label>Text Disabled</label>
+                <input type="text" class="form-control" placeholder="Enter ..." disabled="">
+              </div>
+            </div>
+          </div><div class="row">
+            <div class="col-md-12">
+              <div class="form-group">
+                <label>Text</label>
+                <input type="text" class="form-control" placeholder="Enter ...">
+              </div>
+            </div>
+            <div class="col-md-12">
+              <div class="form-group">
+                <label>Text Disabled</label>
+                <input type="text" class="form-control" placeholder="Enter ..." disabled="">
+              </div>
+            </div>
+          </div><div class="row">
+            <div class="col-md-12">
+              <div class="form-group">
+                <label>Text</label>
+                <input type="text" class="form-control" placeholder="Enter ...">
+              </div>
+            </div>
+            <div class="col-md-12">
+              <div class="form-group">
+                <label>Text Disabled</label>
+                <input type="text" class="form-control" placeholder="Enter ..." disabled="">
+              </div>
+            </div>
+          </div><div class="row">
+            <div class="col-md-12">
+              <div class="form-group">
+                <label>Text</label>
+                <input type="text" class="form-control" placeholder="Enter ...">
+              </div>
+            </div>
+            <div class="col-md-12">
+              <div class="form-group">
+                <label>Text Disabled</label>
+                <input type="text" class="form-control" placeholder="Enter ..." disabled="">
+              </div>
+            </div>
+          </div><div class="row">
+            <div class="col-md-12">
+              <div class="form-group">
+                <label>Text</label>
+                <input type="text" class="form-control" placeholder="Enter ...">
+              </div>
+            </div>
+            <div class="col-md-12">
+              <div class="form-group">
+                <label>Text Disabled</label>
+                <input type="text" class="form-control" placeholder="Enter ..." disabled="">
+              </div>
+            </div>
+          </div><div class="row">
+            <div class="col-md-12">
+              <div class="form-group">
+                <label>Text</label>
+                <input type="text" class="form-control" placeholder="Enter ...">
+              </div>
+            </div>
+            <div class="col-md-12">
+              <div class="form-group">
+                <label>Text Disabled</label>
+                <input type="text" class="form-control" placeholder="Enter ..." disabled="">
               </div>
             </div>
           </div>
@@ -190,4 +264,5 @@ onMounted(async () => {
   </MainContent>
 </template>
 
-<style scoped></style>
+<style scoped>
+</style>
